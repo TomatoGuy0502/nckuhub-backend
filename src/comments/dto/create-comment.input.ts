d95f7f8cab6e730453ca2d842e9e8ec170a956a0
TomatoGuy0502 <H34066131@gs.ncku.edu.tw>
2022-02-08 21:21:53 +0800

    feat: add validation to controller inputs

--- src/comments/dto/create-comment.input.ts
+++ src/comments/dto/create-comment.input.ts
@@ -1,8 +1,30 @@
+import { IsString, Min, Max, IsNotEmpty } from 'class-validator'
+
 export class CreateCommentInput {
+  @IsString()
+  @IsNotEmpty()
   readonly text: string
+
+  @Min(0)
+  @Max(10)
+  @IsNotEmpty()
   readonly got: number
+
+  @Min(0)
+  @Max(10)
+  @IsNotEmpty()
   readonly sweet: number
+
+  @Min(0)
+  @Max(10)
+  @IsNotEmpty()
   readonly cold: number
+
+  @IsString()
+  @IsNotEmpty()
   readonly courseId: string
+
+  @IsString()
+  @IsNotEmpty()
   readonly userId: string
 }
--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,5 +1,15 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator'
+
 export class CreateUserInput {
+  @IsString()
+  @IsNotEmpty()
   readonly facebookId: string
+
+  @IsString()
+  @IsNotEmpty()
   readonly displayName: string
+
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string
 }
